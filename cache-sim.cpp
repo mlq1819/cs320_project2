41af25d35f66b7cb6afb523f2e1d1a2de9e74ec5
Michael Quinnemail <mquinn5@binghamton.edu>
2018-04-25 11:31:23 -0400

    cleaning

--- cache-sim.cpp
+++ cache-sim.cpp
@@ -202,8 +202,8 @@ void DMC::printCache(){
 void DMC::printVars(){
 	cout << "cache_size:\t" << this->cache_size << "\tkB\t|" << endl;
 	cout << "--------------------------------+--------------------------------" << endl;
-	cout << "line_size: \t" << this->line_size << "\tbits\t|\tindex_max: \t" << this->index_max << endl;
-	cout << "index_size:\t" << this->index_size << "\tbits\t|\ttag_max:   \t" << this->tag_max << endl;
-	cout << "tag_size:  \t" << this->tag_size << "\tbits\t|" <<endl;
+	cout << "line_size: \t" << this->line_size << "\tbits\t|"< endl;
+	cout << "index_size:\t" << this->index_size << "\tbits\t|\tindex_max: \t" << this->index_max << endl;
+	cout << "tag_size:  \t" << this->tag_size << "\tbits\t|\ttag_max:   \t" << this->tag_max << endl;
 	cout << "numLines():\t" << this->numLines() << "\tlines\t|\n\n" << endl;
 }
