addb8bc71bdba511b88d3e75a5a0d4ee1ee82675
Michael Quinnemail <mquinn5@binghamton.edu>
2018-04-27 17:09:21 -0400

    fixing issues

--- cache-sim.cpp
+++ cache-sim.cpp
@@ -148,16 +148,10 @@ void DMC::setSizesAndMaxes(){
 	this->tag_max=1;
 	for(unsigned int i=1; i<=this->tag_size; i++)
 		this->tag_max=this->tag_max*2;
-	this->index_offset_size=(4-this->index_size%4)%4;
-	this->index_offset_max=1;
-	for(unsigned int i=1; i<=this->index_offset_size; i++)
-		this->index_offset_max*=2;
-	this->tag_offset_size=(4-this->index_offset_size)%4;
-	this->tag_offset_max=1;
-	for(unsigned int i=1; i<=this->tag_offset_size; i++){
-		this->tag_offset_max*=2;
-		this->tag_max*=2;
-	}
+	this->offset_size=5;
+	this->offset_max=1;
+	for(unsigned int i=1; i<=this->offset_size; i++)
+		this->offset_max*=2;
 }
 
 unsigned long DMC::maxAddress() const {
@@ -186,10 +180,10 @@ double DMC::run(){
 
 bool DMC::step(){
 	Line current = this->reader->current();
-	unsigned long index = (current.getAddress()>>5)%this->index_max;
+	unsigned long index = (current.getAddress()>>this->offset_size)%this->index_max;
 	if(DEBUG && index>=this->index_max)
 		cout << "WARNING: index out of bounds at 0x" << hex << current.getAddress() << ": 0x" <<  hex << index << ">=0x" << hex << this->index_max << dec << endl;
-	unsigned long tag = ((current.getAddress()>>5)>>this->index_size);
+	unsigned long tag = ((current.getAddress()>>this->offset_size)>>this->index_size);
 	/*if(DEBUG && index>=this->index_max)
 		cout << "WARNING: tag out of bounds at 0x" << hex << current.getAddress() << ": 0x" << hex << tag << ">=0x" << hex << this->tag_max  << dec<< endl;
 	*/
@@ -321,6 +315,5 @@ void DMC::printVars(){
 	cout << "line_size:  \t" << dec << this->line_size << "\tbits\t| maxAddress:\t0x" << hex << this->maxAddress() << dec << endl;
 	cout << "index_size: \t" << dec << this->index_size << "\tbits\t| index_max: \t0x" << hex << this->index_max << dec << endl;
 	cout << "tag_size:   \t" << dec << this->tag_size << "\tbits\t| tag_max:   \t0x" << hex << this->tag_max << dec << endl;
-	cout << "IDXOFFSIZE: \t" << dec << this->index_offset_size << "\tbits\t| IDXOFFMAX: \t0x" << hex << this->index_offset_max << dec << endl;
-	cout << "TAGOFFSIZE: \t" << dec << this->tag_offset_size << "\tbits\t| TAGOFFMAX: \t0x" << hex << this->tag_offset_max << dec << endl;
+	cout << "offset_size:\t" << dec << this->offset_size << "\tbits\t| offset_max: \t" << hex << this->offset_max << dec << endl;
 }
